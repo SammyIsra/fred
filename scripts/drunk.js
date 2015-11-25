5b7c92a2de77ef04f537493f1be24c181c6c6d4e
Sammy Israwi <sammy.israwi@gmai.com>
2015-11-25 22:01:11 +0000

    Added rolls. I think I fixed the issue with the sleep timer.

--- scripts/drunk.js
+++ scripts/drunk.js
@@ -1,6 +1,12 @@
+//Descripton:
+//	Tell you if Fred is drunk
+//Commands:
+//	fred are you drunk?
+
 module.exports = function(robot) {
 
-	robot.respond(/are you drunk/i, function(msg){
-		msg.reply("Yes");
+	robot.respond(/are you drunk(\?)?/i, function(msg){
+		console.log(msg);
+		msg.send("Yes");
 	});
 }
---
+++ scripts/roll.js
@@ -0,0 +1,26 @@
+//Description:
+//	Ask fred to roll some dice
+//Commands:
+//	fred roll [1-2000]d[2-2000]
+
+
+module.exports = function(robot){
+
+	robot.respond(/(?:roll (\d{1,4})+d(\d{1,4})+)/i, function(msg){
+
+		console.log("Got: ");
+		for(i in msg.match)
+			console.log(i + ": " + msg.match[i]);
+		
+		if(msg.match[1] < 1 || msg.match[1] > 2000 || msg.match[2] < 2 || msg.match[2] > 2000){
+			msg.reply("Sorry, I cannot calculate that"); 
+		}else{
+			var roll = 0;
+			for(var i = 0 ; i < msg.match[1] ; i++){
+				roll += Math.floor((Math.random() * msg.match[2]) + 1);
+			}
+			msg.reply("You rolled a: " + roll); 
+		}
+
+	});
+}
