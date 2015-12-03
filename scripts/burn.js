008900afefe19a2969a674a46ccfa98678efe1a7
Sammy Israwi <sammy.israwi@gmai.com>
2015-12-03 22:21:27 +0000

    Added more burn material

--- scripts/burn.js
+++ scripts/burn.js
@@ -17,7 +17,9 @@ module.exports = function(robot){
 				"http://assets.nydailynews.com/polopoly_fs/1.2169911!/img/httpImage/image.jpg_gen/derivatives/gallery_1200/drake.jpg",
 				"https://s-media-cache-ak0.pinimg.com/originals/77/da/5f/77da5fee6ad0d1e6419159cf843656d7.gif",
 				"https://media.giphy.com/media/AJwnLEsQyT9oA/giphy.gif",
-				"http://static.comicvine.com/uploads/original/6/69852/3494642-bth_burn_zps519e93fb.gif"
+				"http://static.comicvine.com/uploads/original/6/69852/3494642-bth_burn_zps519e93fb.gif",
+				"get rekt, son",
+				"https://media.giphy.com/media/3rgXBLrnjPRz3Q2Euc/giphy.gif"
 			];
 		
 		msg.send(burn[Math.floor(Math.random()*burn.length)]);
