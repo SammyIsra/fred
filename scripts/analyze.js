e7fb35e19548e25b84ca2bfb9fd2db782d7c15b5
Sammy I <sammy.israwi@gmail.com>
2017-02-14 01:15:51 +0000

    Added error handling and now the analyzer works, but only for text

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -3,17 +3,39 @@
 //Commands:
 //	fred analyze [text]
 
+var Watson = require("./modules/Watson");
+
 
 module.exports = function(robot){
 
 	robot.respond(/(?:analyze\s(?:me)?)(.+)/i, function(msg){
 		
+		//console.log(msg)
+		
 		msg.send(`Gotchu. Will analize the text "${msg.match[1].trim()}"`);
 		
+		Watson.analyzeText(msg.match[1].trim())
+		.then(function(data){
+			console.log(data);
+			var message = "\n*The message that you asked me to analyze has the following scores:*";
+			data.document_tone.tone_categories.forEach((category) => {
+				message += `\n\n*For the ${category.category_name}*`;
+				category.tones.forEach((tone)=>{
+					message += `\n${tone.tone_name}: ${tone.score*100}%`;
+				});
+			});
+			
+			//Respond to caller
+			msg.respond(msg.envelope.user.name + message);
+		})
+		.catch(function(err){
+			console.log(msg.envelope.user.name + " Error happened! " + err);
+			msg.respond(err);
+		});
 		//(need to set Watson keys and new project)
 		//Make call to Watson
 		//Respond to caller
-		//msg.respond
+		//msg.respond  
 	});
 
 }
