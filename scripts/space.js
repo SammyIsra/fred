c978381fc5d1b27637c96673fa6f3a568f702559
Sammy Israwi <sammy.israwi@gmail.com>
2017-12-23 18:16:14 -0500

    Fred can now check how many people (and who is) in space!

---
+++ scripts/space.js
@@ -0,0 +1,44 @@
+//Description
+//  Get information about people in space
+//Commands
+//  fred who is in space (<optional> right now)
+//  fred where is the ISS
+
+const axios = require("axios");
+const PeopleInSpaceUrl = "http://api.open-notify.org/astros.json";
+const ISSLocationUrl = "http://api.open-notify.org/iss-now.json";
+
+module.exports = function(robot){
+
+  robot.respond( /(who is in space\??)/i, function(msg){
+
+    msg.send("Getting that for you, one second...");
+
+    GetPeopleInSpace(PeopleInSpaceUrl)
+      .then( data => {
+
+        if(data === null)
+          return msg.send("Uh oh, there was an error with the response");
+
+        let message = `There are currently ${data.number} people in space! These people are:`;
+
+        for(let person of data.people)
+          message += `\n-${person.name}, aboard the ${person.craft}`;
+
+        msg.send(message);
+      })
+      .catch( err => msg.send(`Uh oh, there was an error with the call...\n${err.message}`));
+    });
+
+}
+
+function GetPeopleInSpace(url){
+  return new Promise( (resolve, reject) => {
+    axios.get(url)
+      .then( resp => resp.data )
+      .then( data => {
+        return resolve(data.message === "success" ? data : null)
+      })
+      .catch( err => reject(data));
+  });
+}
