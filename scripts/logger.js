72cbd82e9ff0453ec3e2394eaf3e90ed80155de4
Sammy Israwi <sammy.israwi@gmail.com>
2018-05-02 01:12:31 -0400

    Hotfix for Hubot build error

--- scripts/logger.js
+++ scripts/logger.js
@@ -2,11 +2,10 @@
 //  This script logs all Fred requests on the Heroku logs. It has no use in chat
 module.exports = function(robot){
 
-	robot.listenerMiddleware(function(context, next){
+	robot.listenerMiddleware(function(context, next, done){ // eslint-disable-line no-unused-vars
 
 		robot.logger.info(context.response.message.user.name + ": " + context.response.message.text);
 		next();
 	});
 
 };
-
